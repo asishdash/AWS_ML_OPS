6ef9540dca6e4afac5ade4aa41fe08bf89bd7be8
asishdash <info@lazyswift.in>
2024-06-09 00:05:22 +0530

    commit for pickel

--- streamlit_demo/demo_streamlit.py
+++ streamlit_demo/demo_streamlit.py
@@ -3,6 +3,7 @@ from sklearn.datasets import fetch_openml
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import pickle as pkl
 
 
 housing_dataset = fetch_openml(name="house_prices",as_frame=True)
@@ -19,6 +20,9 @@ df_new = pd.DataFrame(data=housing_dataset.data, columns=housing_dataset.feature
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 model = LinearRegression()
 model.fit(X_train, y_train)
+with open("model.pkl",'wb') as f :
+    pkl.dump(model,f)
+
 st.title('Ames Housing Price Prediction')
 GrLivArea = st.number_input('Above grade living area in square feet', value=1500)
 YearBuilt = st.number_input('Original construction date', value=2000)
