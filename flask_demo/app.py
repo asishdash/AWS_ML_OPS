d9a1cebb0b4e3ef54232a54d9b9ecab75d5e5ae8
asishdash <info@lazyswift.in>
2024-06-11 23:29:50 +0530

    code check in

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -1,12 +1,24 @@
 from config import DevelopmentConfig, ProductionConfig, TestingConfig
+from flask import Flask
 
 def create_app(config_class):
     app = {} # This would be your Flask or any other app instance in a real scenario app['config'] = config_class
     return app
 # Example of using different configurations
 
+env=''
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello Flask'
+
 if __name__ =="__main__":
     env = 'development' # Change this to 'production' or 'testing' as needed
+    app.run(debug=True)
+
 if env == 'production':
     app = create_app (ProductionConfig)
 elif env == 'testing':
@@ -18,3 +30,5 @@ print("DEBUG: {app['config'].DEBUG}")
 print("DATABASE_URI: {app['config'].DATABASE_URI}")
 print("SECRET_KEY: {app['config'].SECRET_KEY}")
 print("API_KEY: {app['config'].API_KEY}")
+
+
